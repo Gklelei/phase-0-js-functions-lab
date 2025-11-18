9a3857e12acb60177f9f1bb64e5b8578ea40a7b7
gklelei <leleigideon97@gmail.com>
2025-11-18 12:27:53 +0300

    practiced on creating functions and run test command and my functions passed

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
+function calculateTax(amount) {
+  const tax = amount * 0.1;
 
+  return tax;
+}
 
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
 
+function findMaximum(num1, num2) {
+  let max = Math.max(num1, num2);
 
+  return max;
+}
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+function isPalindrome(word) {
+  let palindrom = word.toLowerCase().split("").reverse().join("");
+
+  return palindrom === word;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discount = originalPrice * (discountPercentage / 100);
+  const discountedPrice = originalPrice - discount;
+
+  return discountedPrice;
+}
+// This is required for the test to function properly
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice,
+};
